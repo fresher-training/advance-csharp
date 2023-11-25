202f4f142399ecca4e1747e7a5a049bf8227caa5
vosonha89 <vosonha89@gmail.com>
2023-11-25 19:13:24 +0700

    add doc

--- advance-csharp-dummy/ProductExtension.cs
+++ advance-csharp-dummy/ProductExtension.cs
@@ -2,6 +2,10 @@
 
 namespace advance_csharp_dummy
 {
+    /// <summary>
+    /// Documentation
+    /// https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/extension-methods
+    /// </summary>
     public static class ProductExtension
     {
         public static long SummaryTotalPrice(this Product product)
